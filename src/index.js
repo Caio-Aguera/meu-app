2a4a2f03d72bef6c0fa01482e1b2e4cd5fe35286
Caio-Aguera <168736532+Caio-Aguera@users.noreply.github.com>
2024-10-24 12:32:44 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Relogio from './Relogio';
-import Letreiro from './Letreiro'
+import Relogio from '../../src/Relogio';
+import Letreiro from '../../src/Letreiro';
+import Contador from '../../src/Contador';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <>
  <Relogio/>
  <Letreiro/>
+ <Contador/>
  </>
 );
